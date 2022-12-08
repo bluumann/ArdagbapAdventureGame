49f114942e168439dbb1db78a0782cbcedb3a372
Peter Kosa <p.kosa089@mybvc.ca>
2022-12-08 16:41:54 -0700

    Add Card and Profile classes with properties and methods

---
+++ ArdagbapAdventureGame/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArdagbapAdventureGame
+{
+    public class Card
+    {
+        public string CardName { get; set; }
+        public string CardType { get; set; }
+        public Image  CardImage { get; set; }
+        public string CardDescription { get; set; }
+        public bool IsTemporary { get; set; }
+        public string CardEffect { get; set; }
+        public int CardPower { get; set; }
+    }
+}
--- ArdagbapAdventureGame/Profile.cs
+++ ArdagbapAdventureGame/Profile.cs
@@ -1,6 +1,34 @@
-﻿namespace ArdagbapAdventureGame
+﻿using System.Collections.Generic;
+
+namespace ArdagbapAdventureGame
 {
-    internal class Profile
+    public class Profile
     {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int CurrentHealth { get; set; }
+        public int MaxHealth { get; set; }
+        public int Shielding { get; set; } = 0;
+        public int Gold { get; set; }
+        public List<Card> Deck { get; set; }
+        public List<Card> AvailableCards { get; set; }
+        public int CurrentAdventure { get; set; } = 1;
+        public int AdventureLevel { get; set; } = 1;
+
+
+        public void SaveProfile()
+        {
+
+        }
+
+        public void UpdateDeck()
+        {
+
+        }
+
+        public void CreateNewDeck()
+        {
+
+        }
     }
 }
