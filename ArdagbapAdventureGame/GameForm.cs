882d3eaf165b078abd467570a361b432d75d7daa
Pedro Ferreira <yami_los@hotmail.com>
2022-12-10 10:47:19 -0700

    Merge fix.

--- ArdagbapAdventureGame/GameForm.cs
+++ ArdagbapAdventureGame/GameForm.cs
@@ -17,18 +17,12 @@ namespace ArdagbapAdventureGame
             InitializeComponent();
         }
 
-<<<<<<< HEAD
         private void btnExit_Click(object sender, EventArgs e)
         {
             MainMenu mainMenu = new MainMenu();
             this.Hide();
             mainMenu.ShowDialog();
             this.Close();
-=======
-        private void cardCC1_Load(object sender, EventArgs e)
-        {
-
->>>>>>> c0ca92d5cbdf71f3cc919bb54d9ab29265b085b6
         }
     }
 }
