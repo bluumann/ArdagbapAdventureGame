c17f1282b61b53a626ab92ab01cf1914eb977b08
Pedro Ferreira <yami_los@hotmail.com>
2022-12-12 17:58:32 -0700

    Finalized combat and dialog text (phewwww)!

--- ArdagbapAdventureGame/CreatureCombat.cs
+++ ArdagbapAdventureGame/CreatureCombat.cs
@@ -28,19 +28,19 @@ namespace ArdagbapAdventureGame
                     switch (key)
                     {
                         case 0:
-                            EventDescription = "Wizard Case0: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "A mysterious figure materializes from the shadows. They look at you menacing and their hands begin sporting an eerie glow. What stands before you is clearly a Wizard or Sorceror, ready to blast you into oblivion with spells and incantations.";
                             break;
                         case 1:
-                            EventDescription = "Wizard Case1: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "A nearby portal shimmer with light. On it's inside, you can see a clearing with nice trees and yellow butterflies. As you gaze up to marvel at their beauty, the portal colapses into a hellish landscape and a furious foe comes through.";
                             break;
                         case 2:
-                            EventDescription = "Wizard Case2: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "A pool of ooze smells nearby. As you approach to see what's inside, it seems to dissolve anything that falls into it. As you come even close, the pool churls and jumps towards you, revealing itself to be a slime creature taking a more distinguishable form.";
                             break;
                         case 3:
-                            EventDescription = "Wizard Case3: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "You wake up... But you don't recall sleeping. As you look around you, you hear a laughter as the seemingly witch-like figure stands behind you hands raised with a glimmering potion and a shimmering sigil. The witch crackles at you and proceeds to attack viciously.";
                             break;
                         case 4:
-                            EventDescription = "Wizard Case4: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "A child wearing armor comes running at you. Upon closer inspection you realize it's not a child, but a halfling cleric. She screams and points towards a tenebrous figure that chasing her. The lich approaches and you take up your arms beside your newfound ally to fight the evil force!";
                             break;
                         default:
                             EventDescription = "Wizard A generic event description has been generated." + "KEY WAS:" + key;
@@ -51,19 +51,19 @@ namespace ArdagbapAdventureGame
                     switch (key)
                     {
                         case 0:
-                            EventDescription = "Rogue Case0: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "As you walk by in your quest, you hear a faint hustling of leaves. As you turn to check, you see a silhoute amongst the trees. You are being chased by a some brigand or rogue-like being. Before you can do anything, you realize you are surrounded and take up your arms.";
                             break;
                         case 1:
-                            EventDescription = "Rogue Case1: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "You fall into a trap and spring your ankle. However, worse than that, you realize the trap is also home to an assassin who was waiting to ambush you. It's time to fight or die.";
                             break;
                         case 2:
-                            EventDescription = "Rogue Case2: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "You meet a strange in the road. As he offers you some sweet candy, you remember you mom's words that you should never take candy from strangers. When denied, the stranger begins to act agressively and decides to shank you. You quickly defend yourself and steel up for combat!";
                             break;
                         case 3:
-                            EventDescription = "Rogue Case3: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "You see a small sign in the street. Upon getting close, you read: This is a generic event and is going to trigger a combat encounter. Before you can make anything of the message, you are sprung into action by the sound of flying daggers. They almost hit you as you turn to face an assassin sent to kill you.";
                             break;
                         case 4:
-                            EventDescription = "Rogue Case4: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "You begin to hear a squeaky sound getting close and closer. It reminds you of the shoes clowns used to wear on the parties in your childhood. As the squeakyness gets more annoying, you realize you are being followed by a mysterious figure in clown shoes. What does it want you ask, as it looks towards you getting close and close without uttering a single word. When the clownish figure is at arms reach, you decide to act wether or not you should.";
                             break;
                         default:
                             EventDescription = "Rogue A generic event description has been generated." + "KEY WAS:" + key;
@@ -74,19 +74,19 @@ namespace ArdagbapAdventureGame
                     switch (key)
                     {
                         case 0:
-                            EventDescription = "Warrior Case0: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "You hear a faint cheer over the distance. Someone is remarking on what is good in life and apparently, it is to crush your enemies, see them driven before you and hear the lamentations of their women. As you approach, you realize a gigantic towering figure is about to challenge you to combat!";
                             break;
                         case 1:
-                            EventDescription = "Warrior Case1: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "You stand before a shrine. You remember those who wronged you in the past. As you remember you are not the praying kind, you just state your words to Crom, asking for justice. Before you can finish, you hear the footsteps of your old nemesis, as Crom himself seems to be delivering your change for revenge.";
                             break;
                         case 2:
-                            EventDescription = "Warrior Case2: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "The smell of graham crackers fills the air. A loud and fuzzy creature approaches and looks at you. It seems almost decided to tank you and everything you build with your sweat and tears. You decide to fight back before this thing takes from you all you love.";
                             break;
                         case 3:
-                            EventDescription = "Warrior Case3: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "On the distance, you see a Goliath with a gigantic war hammer. As he turns to you, his bare chest gleaming against the sunlight, he exclaims that he shall be the greatest of the Garbazi and you shall be dead!";
                             break;
                         case 4:
-                            EventDescription = "Warrior Case4: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription = "You hear a faint whistling of wind. A quick-footed warrior dashes towards you, his skin almost as if made of minerals, his eyes gleaming with a fiery force. The quartz-like warrior seems as eager to prove himself in combat as he is to shine in his arts. You have no choice, but to defend yourself!";
                             break;
                         default:
                             EventDescription = "Warrior A generic event description has been generated." + "KEY WAS:" + key;
--- ArdagbapAdventureGame/DialogEncounter.cs
+++ ArdagbapAdventureGame/DialogEncounter.cs
@@ -13,26 +13,26 @@ namespace ArdagbapAdventureGame
         public DialogEncounter(string eventName, Image eventImage, string eventType, int key) : base(eventName, eventImage, eventType)
         {
             EventDescription = "";
-            if (eventName == "A Dank and Dark Prison") EventDescription = "Placeholder Starting" + " ";
+            if (eventName == "A Dank and Dark Prison") EventDescription = "You wake up on a dank and dark prison, as you look around you realize you've been tricked by your old foe, as he went for the Temple of the Great Destroyer. You can only hope, while he betrayed you, he still wants to prevent the destroyer's release." + " ";
             switch (eventType)
             {
                 case "Spell":
                     switch (key)
                     {
                         case 0:
-                            EventDescription += "Spell Case0: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "You see floating fireflies, they seem to shimmer with a odd magical glow. As you approach one of the bugs, you realize it is reacting to you. The whole swarm seems to be some sort of alarm system to keep you from moving forward. You'll have to move carefuly through them to avoid detection.";
                             break;
                         case 1:
-                            EventDescription += "Spell Case1: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "You notice a nearby surface covered in runes. It is clearly a trap! Perhaps some other runes are hidden on the floor or surfaces around you, better be careful.";
                             break;
                         case 2:
-                            EventDescription += "Spell Case2: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "You hear the loud roar of a dragon in the distance, if it notices you, you will sure be dead.";
                             break;
                         case 3:
-                            EventDescription += "Spell Case3: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "You find yourself surrounded by a magical forcefield, you know not how it activated but you realize it comes from an arcane lockbox hidden on a nearby rock. Perhaps you should try to open the box to disable the forcefield?";
                             break;
                         case 4:
-                            EventDescription += "Spell Case4: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "Complex swirling arcane symbols float all around you. Every one of them seems to react to you harmlessly. However, when trying to go through them, they lock and solidify themselves. Perhaps you'll have to arrange them in the correct order to pass?";
                             break;
                         default:
                             EventDescription += "Spell A generic event description has been generated." + "KEY WAS:" + key;
@@ -43,19 +43,19 @@ namespace ArdagbapAdventureGame
                     switch (key)
                     {
                         case 0:
-                            EventDescription += "Skill Case0: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "Two guards stand before you. They say one of them always lie, the other always tells the truth. You remember and old tale of the mighty Garbazi, who when faced with this dilema, ripped the head of one of the guards and asked the other, is he dead?";
                             break;
                         case 1:
-                            EventDescription += "Skill Case1: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "A wooden donkey wheel sits between you and you path. You realize to pass, you'll need to turn the wheel as once the mighty Nanoc did. That is good, that is good.";
                             break;
                         case 2:
-                            EventDescription += "Skill Case2: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "You see an old ghost. You scream and tell him to go away. It doesn't. It starts to ramble about some sort of old warrior and some higiene process call Dago-Bath. You don't understand anything, but the ghost tells you to use the force! What force? Maybe he means your might!";
                             break;
                         case 3:
-                            EventDescription += "Skill Case3: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "As you venture further into the depths of your surroundings, you realize an old monk is exiting a nearby dungeon. He is carrying a lot of junk and gently asks for you help to carry everything. Before you depart on your way, perhaps you should help the old man, no?";
                             break;
                         case 4:
-                            EventDescription += "Skill Case4: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "You hear a gigantic boulder rolling down the hill, you see it's going to hit a nearby house. You remember Nanoc, you scream, NANOC! Perhaps you should intercept the boulder with all your might!";
                             break;
                         default:
                             EventDescription += "Skill A generic event description has been generated." + "KEY WAS:" + key;
@@ -66,19 +66,19 @@ namespace ArdagbapAdventureGame
                     switch (key)
                     {
                         case 0:
-                            EventDescription += "Strength Case0: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "A gigantic golem blocks the way. It is immobile and indestructible. Perhaps he would answer to his master or the mages of old. Luckily, you remember you have a few tricks on your sleeve.";
                             break;
                         case 1:
-                            EventDescription += "Strength Case1: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "You hear a nearby bard, telling a story about how a mysterious entity was defeated. And the their soulstones were destroyed into the hell forge... All except one. The bard reveals a small, glimmering yellow stone. Nearby, a wandering stranger begins to approach and reach for the stone. You realize, it's not the Archangel Tyrael, but Baal. You must act to destroy the stone before he takes it!"; 
                             break;
                         case 2:
-                            EventDescription += "Strength Case2: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "Six orbs of shimmer on the distance, mirrored by a single light. Creatio Ex Nihilo, you realize as you are blessed. You open up your soul to them, basking as the Redeemer, the Tempest, the Baron, the Doctor, the Maelstrom and the Empress smile at you. At a distance, a Wolf howls inconspicuously. You feel your magical energy bursting and ready to take you even further.";
                             break;
                         case 3:
-                            EventDescription += "Strength Case3: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "A golden miasma flows from everywhere, you realize time is shifting and quaking. On the golden road of nothingness, for a second that never was, the Empress That Always Been and her Golden Army of Meanwhiles and Always Weres spearhead into the void, calling heroes and wolves alike in a fight for hope and creation. Was it a dream? Did it really happen? You don't know, but you feel like you should leave some of your magic here, as to serve the bearers of gifts.";
                             break;
                         case 4:
-                            EventDescription += "Strength Case4: A generic event description has been generated." + "KEY WAS:" + key;
+                            EventDescription += "You see a nearby tablet with an odd prophecy written on it. As you begin to read, your eyes swirl into a reddish form with pointy dots. You realize your blood is boiling with magic and you must contain the power before you're lost in a kaleidoscopic maze of infinte dreams.";
                             break;
                         default:
                             EventDescription += "Strength A generic event description has been generated." + "KEY WAS:" + key;
