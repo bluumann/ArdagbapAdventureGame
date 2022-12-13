036bde76eb39ce10048ca17ea13ce4c8410a297d
Peter Kosa <p.kosa089@mybvc.ca>
2022-12-12 21:32:39 -0700

    Add updated profiles to Main Menu

--- ArdagbapAdventureGame/MainMenu.cs
+++ ArdagbapAdventureGame/MainMenu.cs
@@ -12,6 +12,12 @@ namespace ArdagbapAdventureGame
 {
     public partial class MainMenu : Form
     {
+        // Containing all "ProfileControl" instances of profiles created by the user
+        public static List<ProfileControl> profileControlList = new List<ProfileControl>();
+
+        // The currenlty selected ProfileControl
+        public static ProfileControl selectedProfileControl;
+
         GameForm gameForm;
         public MainMenu()
         {
@@ -45,6 +51,30 @@ namespace ArdagbapAdventureGame
         private void MainMenu_Load(object sender, EventArgs e)
         {
             
+        }
+
+        public static void HandleProfileControlSelection()
+        {
+            foreach (var pf in profileControlList)
+            {
+                if (pf != selectedProfileControl)
+                {
+                    pf.BackColor = Color.DarkGray;
+                    // Set text color for all labels
+                    pf.lblProfileNameText.ForeColor = SystemColors.ControlText;
+                    pf.lblProfileGenderText.ForeColor = SystemColors.ControlText;
+                    pf.lblProfileGoldText.ForeColor = SystemColors.ControlText;
+                    pf.lblProfileCurrentAdText.ForeColor = SystemColors.ControlText;
+                    pf.lblProfileAdLevelText.ForeColor = SystemColors.ControlText;
+
+                    // Set text color for all values
+                    pf.lblProfileName.ForeColor = SystemColors.ControlText;
+                    pf.lblProfileGender.ForeColor = SystemColors.ControlText;
+                    pf.lblProfileGold.ForeColor = SystemColors.ControlText;
+                    pf.lblProfileCurrentAd.ForeColor = SystemColors.ControlText;
+                    pf.lblProfileAdLevel.ForeColor = SystemColors.ControlText;
+                }
+            }
         }
     }
 }
--- ArdagbapAdventureGame/Properties/Settings.Designer.cs
+++ ArdagbapAdventureGame/Properties/Settings.Designer.cs
@@ -13,7 +13,7 @@ namespace ArdagbapAdventureGame.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
 
@@ -24,6 +24,19 @@ namespace ArdagbapAdventureGame.Properties
             get
             {
                 return defaultInstance;
+            }
+        }
+
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;" +
+            "Integrated Security=True")]
+        public string connString
+        {
+            get
+            {
+                return ((string)(this["connString"]));
             }
         }
     }
